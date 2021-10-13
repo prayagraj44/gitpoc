804005277951a5a0eef3f72c0a85433d1d525143
prayag <prayagraj44@gmail.com>
2021-10-13 23:08:06 +0530

    initial commit

---
+++ src/main/java/com/prayag/gitPoc/GitPocApplication.java
@@ -0,0 +1,13 @@
+package com.prayag.gitPoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitPocApplication.class, args);
+	}
+
+}
